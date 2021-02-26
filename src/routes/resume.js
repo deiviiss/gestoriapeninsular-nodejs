2cddb0e4bdf1627104babcd107ed3dc5a0ed0bbe
David Hilera <david.hilera@hotmail.com>
2021-02-26 15:52:55 -0600

    Nabar float, vista resumen, footer

--- src/routes/resume.js
+++ src/routes/resume.js
@@ -12,36 +12,89 @@ const helpers = require('../lib/handlebars')
 router.get('/resume', isLoggedIn, async (req, res) => {
   const user = req.user
 
-  console.log(user.consulta)
-
   // customers = await pool.query("SELECT status, COUNT(*) as total FROM tramites WHERE zona like '%" + [req.user.consulta] + "%'GROUP BY status")
 
-  customers = await pool.query("SELECT status, COUNT(*) as total FROM tramites WHERE zona like '%" + [req.user.consulta] + "%'GROUP BY status")
+  //condición para determinar el usuario
+  if (user.consulta === "") {
 
-  rows = customers
+    aclaraciones = await pool.query("SELECT * FROM tramites WHERE status= 'aclaración'")
 
-  console.log(rows)
+    asegurados = await pool.query("SELECT * FROM tramites WHERE status= 'asegurado'")
 
-  console.log(customers[0].status);
-  console.log(customers[1].status);
-  console.log(customers[2].status);
-  console.log(customers[3].status);
-  console.log(customers[4].status);
-  console.log(customers[5].status);
-  console.log(customers[6].status);
+    bajas = await pool.query("SELECT * FROM tramites WHERE status= 'baja'")
 
-  // status = {
-  //   aclaraciones: aclaraciones.length,
-  //   bajas: bajas.length,
-  //   espera: espera.length,
-  //   fallidos: fallidos.length,
-  //   finalizados: finalizados.length,
-  //   pendientes: pendientes.length
-  // }
+    espera = await pool.query("SELECT * FROM tramites WHERE status= 'en espera'")
 
-  res.send('Pendientes ' + customers.status)
+    fallidos = await pool.query("SELECT * FROM tramites WHERE status= 'fallido'")
 
-  // res.render('customer/list.hbs', { customer })
+    finalizados = await pool.query("SELECT * FROM tramites WHERE status= 'finalizado'")
+
+    juridico = await pool.query("SELECT * FROM tramites WHERE status= 'jurídico'")
+
+    pendientes = await pool.query("SELECT * FROM tramites WHERE status= 'pendiente'")
+
+  } else {
+    aclaraciones = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'aclaración'", [req.user.consulta])
+
+    asegurados = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'asegurado'", [req.user.consulta])
+
+    bajas = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'baja'", [req.user.consulta])
+
+    espera = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'en espera'", [req.user.consulta])
+
+    fallidos = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'fallido'", [req.user.consulta])
+
+    finalizados = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'finalizado'", [req.user.consulta])
+
+    juridico = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'jurídico'", [req.user.consulta])
+
+    pendientes = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status= 'pendiente'", [req.user.consulta])
+  }
+
+  //objeto con status que se mandara a la vista
+  status = {
+    aclaraciones: aclaraciones.length,
+    asegurados: asegurados.length,
+    bajas: bajas.length,
+    espera: espera.length,
+    fallidos: fallidos.length,
+    finalizados: finalizados.length,
+    juridico: juridico.length,
+    pendientes: pendientes.length
+  }
+
+  //objeto con titulos de status
+  titulos = {
+    aclaracion: 'aclaracion',
+    asegurado: 'asegurado',
+    baja: 'baja',
+    espera: 'en espera',
+    fallido: 'fallido',
+    finalizado: 'finalizado',
+    juridico: 'jurídico',
+    pendiente: 'pendiente'
+  }
+
+  res.render('customer/resume.hbs', { status, user, titulos })
+})
+
+router.get('/resume/:status', isLoggedIn, async (req, res) => {
+  const { status } = req.params
+  const user = req.user
+
+  console.log('Recibido el ' + status);
+
+  if (user.consulta === "") {
+    customer = await pool.query("SELECT * FROM tramites WHERE status = ?", [status])
+  } else {
+    customer = await pool.query("SELECT * FROM tramites WHERE zona = ?" + " AND  status = ?", [user.consulta, status])
+  }
+
+  //helper que cambia el formato de fecha y moneda
+  customers = helpers.formatterCustomers(customer)
+
+  res.render('customer/list.hbs', { customer })
+
 })
 
 module.exports = router;
